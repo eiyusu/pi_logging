7bac79b26018f144963a78e0d18dbca7578abdb6
Victor <victor.gralmeida@gmail.com>
2022-11-04 08:35:02 -0300

    Update read_log.py
    Added append n init msg

--- read_log.py
+++ read_log.py
@@ -2,7 +2,9 @@ import serial
 import logging
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
-logging.basicConfig(filename="log.txt", level=logging.DEBUG, format="%(asctime)s %(message)s")
+logging.basicConfig(filename="log.txt", level=logging.DEBUG, format="%(asctime)s %(message)s", filemode='a')
+
+logging.debug("Reinicialização da Pi")
 
 while True:
     cc=str(ser.readline())
